515be723f154b18c34ab1249cf135304547d3e6c
arashiaslan <imaqil.mj@gmail.com>
2024-09-18 23:11:37 +0700

    kelarr

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const inBox = document.getElementById("in-box");
+const listMu = document.getElementById("listmu");
+
+function tambahTugas() {
+    if (inBox.value === '') {
+        Swal.fire({
+            icon: "error",
+            title: "Sing mau Isien sek!",
+            text: "Ra diisi kok pencet tambah, meh nambah opo?!",
+        });
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inBox.value;
+        listMu.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inBox.value = "";
+    simpenKi();
+}
+
+listMu.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("uwis");
+        simpenKi();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        simpenKi();
+    }
+}, false);
+
+function simpenKi() {
+    localStorage.setItem("data", listMu.innerHTML);
+}
+
+function delokKe() {
+    listMu.innerHTML = localStorage.getItem("data");
+}
+
+delokKe();
